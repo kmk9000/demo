a6cdfa52a6a86bacbc2dff7f300fcf89a688b227
kmk9000 <kallekoivuniemi@gmail.com>
2025-11-04 15:06:08 +0200

    add submit form

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,35 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Card from "./components/Card";
 import Counter from "./components/Counter";
+import Form from "./components/Form";
 import { useState } from "react";
 import data from "./data";
 
 function App() {
   const [count, setCount] = useState(0);
   const [employees, setEmployees] = useState(data);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    title: "",
+    salary: "",
+    phone: "",
+    email: "",
+    animal: "",
+  });
 
   const handleClick = () => {
     setEmployees([
       ...employees,
       {
         id: employees.length + 1,
-        name: "John Doe",
-        title: "Developer",
-        salary: "4500",
-        phone: "000-000-0000",
-        email: "john@company.com",
-        anime: "Dog",
+        name: formData.name,
+        title: formData.title,
+        salary: formData.salary,
+        phone: formData.phone,
+        email: formData.email,
+        animal: formData.animal,
+        isFavourite: false,
       },
     ]);
   };
@@ -41,7 +52,6 @@ function App() {
     <div className="container">
       <Header />
       <main>
-        <button onClick={handleClick}>Add Employee</button>
         {employees.map((employee) => (
           <Card
             key={employee.id}
@@ -50,6 +60,11 @@ function App() {
           />
         ))}
         <Counter count={count} setCount={setCount} />
+        <Form
+          formData={formData}
+          setFormData={setFormData}
+          handleClick={handleClick}
+        />
       </main>
       <Footer count={count} setCount={setCount} />
     </div>
---
+++ src/components/Form.jsx
@@ -0,0 +1,86 @@
+import styles from "./Form.module.css";
+
+const Form = ({ formData, setFormData, handleClick }) => {
+  const handleChange = (e) => {
+    setFormData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleClick();
+    setFormData({
+      firstName: "",
+      lastName: "",
+      title: "",
+      salary: "",
+      phone: "",
+      email: "",
+      animal: "",
+    }); // optional reset of the form after submission
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name"> Name</label>
+        <input
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        <label htmlFor="salary">Salary</label>
+        <input
+          id="salary"
+          name="salary"
+          value={formData.salary}
+          onChange={handleChange}
+        />
+        <label htmlFor="phone">Phone</label>
+        <input
+          id="phone"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="animal">Animal</label>
+        <input
+          id="animal"
+          name="animal"
+          value={formData.animal}
+          onChange={handleChange}
+        />
+        <button type="submit">Add employee</button>
+        {/* Alternatively, you can use just button without submit type */}
+        {/* <button type="button" onClick={handleClick}>Add Employee</button> */}
+        <div className={styles.output}>
+          <p>Name: {formData.name}</p>
+          <p>Title: {formData.title}</p>
+          <p>Salary: {formData.salary}</p>
+          <p>Phone: {formData.phone}</p>
+          <p>Email: {formData.email}</p>
+          <p>Animal: {formData.animal}</p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
