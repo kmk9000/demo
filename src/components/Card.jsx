911aac9c213b49a4cae51ef3511c9fcddfd853c6
kmk9000 <kallekoivuniemi@gmail.com>
2025-11-25 10:23:43 +0200

    propt-types

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 import styles from "./Card.module.css";
 import { useNavigate } from "react-router";
+import PropTypes from "prop-types";
 
 export default function Card({
-  name,
+  name = "Example Name",
   title,
   salary,
   phone,
@@ -33,3 +34,16 @@ export default function Card({
     </div>
   );
 }
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  salary: PropTypes.number.isRequired,
+  phone: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  animal: PropTypes.string.isRequired,
+  isFavourite: PropTypes.bool,
+  toggleFavourite: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+};
